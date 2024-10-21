44b212adbecb1eebad2730dcda8b51a9d488c8f3
Really1210 <whchoi86@naver.com>
2024-10-21 05:04:57 +0000

    1404

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ from math import radians, sin, cos, sqrt, atan2
 client_id = "buzzqnu77m"
 client_secret = "QkOrNDd4v57qIR2WKrE1gNO7WKKYeiXUMtjjfTAN"
 
-client_id_1 = "buzzqnu77m"
-client_secret_1 = "QkOrNDd4v57qIR2WKrE1gNO7WKKYeiXUMtjjfTAN"
+client_id_1 = "xbajg8w92p"
+client_secret_1 = "MPSE5rkfxFRJT98AgbzRoidsGBu3xjT1h93tKSac"
 
 # 좌표를 받아오는 함수
 def get_coordinates(address):
@@ -68,7 +68,7 @@ def create_naver_map_url(start_coord, end_coord):
     return None
 
 # Streamlit 앱 UI
-st.title("출발지와 도착지의 거리 및 지도 표시_1402")
+st.title("출발지와 도착지의 거리 및 지도 표시_1404")
 
 start_address = st.text_input("출발 주소 입력")
 end_address = st.text_input("도착 주소 입력")
