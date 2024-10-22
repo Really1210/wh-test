16e74ac5bc2b3db846c2fe787623cdc503051ea4
Really1210 <whchoi86@naver.com>
2024-10-22 07:31:53 +0000

    1630

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,7 +115,7 @@ if st.button("1. 개별 거리 계산"):
                             data,
                             get_position='[lon, lat]',
                             get_color='color',
-                            get_radius=150,  # 원 크기 조정 (단위: 미터)
+                            get_radius=50,  # 원 크기 조정 (단위: 미터)
                             pickable=True
                         )
                         
